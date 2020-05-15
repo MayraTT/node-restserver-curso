c0cd77b9e3bb31929aa39e18e0fd9e32d821e411
MayraNode <mayra.lopez@fumired.com.mx>
2020-05-15 09:39:41 -0500

    Rest server - usuario

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 require('./config/config');
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
 
@@ -7,38 +9,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.get('/usuario', function(req, res) {
-    res.json('get usuario');
-});
+app.use(require('./controladores/usuario'));
 
-app.post('/usuario', function(req, res) {
 
-    let body = req.body;
 
-    if (body.nombre == undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            "persona": body
-        });
-    }
+mongoose.connect(process.env.urlDB, { useNewUrlParser: true, useCreateIndex: true }, (err, res) => {
 
-});
+    if (err) throw err;
 
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
+    console.log('Base de datos online');
 
-app.delete('/usuario', function(req, res) {
-    res.json('delete usuario');
 });
 
 app.listen(process.env.PORT, () => {
